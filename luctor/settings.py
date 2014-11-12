eb2c7e672d55eccd1e9c48bbd28aaf5fc3417179
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-11-12 09:49:40 +0100

    move uploaded files

--- luctor/settings.py
+++ luctor/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# Paths to store database and uploaded files
+# for production, should move uploads somewhere suitable for serving
+DATA_DIR = os.path.join(BASE_DIR, "data")
+MEDIA_ROOT = os.path.join(DATA_DIR, "upload")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -60,7 +64,7 @@ WSGI_APPLICATION = 'luctor.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(DATA_DIR, 'db.sqlite3'),
     }
 }
 
