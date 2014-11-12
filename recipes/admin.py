eed5ce26bf535beb78d659d6b9dfb654722b06be
Wouter van Atteveldt <wouter@vanatteveldt.com>
2014-11-12 09:19:45 +0100

    fancy admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from django import forms
 
 from recipes.models import Lesson
 
 class LessonAdmin(admin.ModelAdmin):
     search_fields = ['title', 'text']
     list_display = ('date', 'title')
+    list_filter = ('date',)
+    date_hierarchy = 'date'
+
 
 admin.site.register(Lesson, LessonAdmin)
