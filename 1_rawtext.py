da93403280f056e72f94fadb0aff69193a58cd44
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-04-03 23:07:15 +0200

    prefer unuconv over odt2txt

--- 1_rawtext.py
+++ 1_rawtext.py
@@ -19,13 +19,12 @@ def get_text(fn):
 
     error.append("antiword: {err}".format(err=err.decode("latin-1")))
     
-    if b'Rich Text Format' in err:
-        cmd = ['/usr/bin/python3', '/usr/bin/unoconv', '-f', 'text', '-e', 'FilterOptions=UTF8,LF', '--stdout', fn] 
-        #cmd = ['unrtf', '--text', fn]
-        p = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-        out, err = p.communicate()
-        if p.returncode == 0:
-            return True, out.decode("utf-8")
+    cmd = ['/usr/bin/python3', '/usr/bin/unoconv', '-f', 'text', '-e', 'FilterOptions=UTF8,LF', '--stdout', fn] 
+    #cmd = ['unrtf', '--text', fn]
+    p = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    if p.returncode == 0:
+        return True, out.decode("utf-8")
         
         #encoding = "latin-1"
         #if p.returncode == 0:
@@ -33,7 +32,7 @@ def get_text(fn):
         #    out = out.split("-----------------", 1)[1].strip()
         #    return True, out
 
-        error.append("unoconv: {err}".format(err=err.decode("latin-1")))
+    error.append("unoconv: {err}".format(err=err.decode("latin-1")))
         
     cmd = ["odt2txt", "--encoding=utf-8", fn]
     p = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -47,14 +46,14 @@ def get_text(fn):
                      
             
 
-for l in Lesson.objects.filter(status=0):
+for l in Lesson.objects.filter(status=2):
     fn = l.docfile.file.file.name
     print(l.id, fn)
 
     success, msg = get_text(fn)
 
     if success:
-        l.status = 1
+        #l.status = 1
         l.raw_text = msg
         l.problems = ""
     else:
